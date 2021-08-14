0e256c0473a10b44d768a7bb5eab56d3d9f8c22e
hackbolt <88914049+hackbolt@users.noreply.github.com>
2021-08-13 21:40:51 -0400

    Bots n Bigots
    Contains the 1.1 bots/bigots.

---
+++ README.md
@@ -0,0 +1,7 @@
+List of twitch bots n bigots
+============================
+
+Fork of [List of twitch bots n bigots](https://github.com/hackbolt/twitchbotsnbigots)
+directly containing a single column sorted UTF-8 encoded CSV file.
+
+Run `update.py` to update list from the original repository.
---
+++ update.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from typing import Set
+from codecs import BOM_UTF8, BOM_UTF16_BE, BOM_UTF16_LE, BOM_UTF32_BE, BOM_UTF32_LE
+
+import io
+import csv
+import zipfile
+
+import requests
+
+URL = 'https://github.com/hackbolt/twitchbotsnbigots/raw/main/Bots%20n%20Bigots.zip'
+
+def decode_using_bom(data: bytes) -> str:
+    if data.startswith(BOM_UTF16_BE):
+        value = data[len(BOM_UTF16_BE):].decode('UTF-16BE', errors='replace')
+
+    elif data.startswith(BOM_UTF16_LE):
+        value = data[len(BOM_UTF16_LE):].decode('UTF-16LE', errors='replace')
+
+    elif data.startswith(BOM_UTF32_BE):
+        value = data[len(BOM_UTF32_BE):].decode('UTF-32BE', errors='replace')
+
+    elif data.startswith(BOM_UTF32_LE):
+        value = data[len(BOM_UTF32_LE):].decode('UTF-32LE', errors='replace')
+
+    elif data.startswith(BOM_UTF8):
+        value = data[len(BOM_UTF8):].decode('UTF-8', errors='replace')
+
+    else:
+        value = data.decode('UTF-8', errors='replace')
+
+    return value
+
+def update() -> None:
+    print(f"Downloading {URL} ...")
+    response = requests.get(URL)
+    response.raise_for_status()
+    bots: Set[str] = set()
+
+    print("Reading file...")
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
+        with zip.open("Bots n Bigots.csv", "r") as fp:
+            data = fp.read()
+
+    text = decode_using_bom(data)
+
+    print("Converting file...")
+    for row in csv.reader(io.StringIO(text)):
+        for cell in row:
+            cell = cell.strip()
+            if cell and 'Compiled by Hackbolt' not in cell:
+                bots.add(cell)
+
+    sorted_bots = sorted(bots)
+
+    print("Writing new file...")
+    with open("bots_n_bigots.csv", "w") as outfp:
+        csv.writer(outfp).writerows([bot] for bot in sorted_bots)
+
+if __name__ == '__main__':
+    update()
